68a748fb28cfb8847cd3d8235e5763b6a75c5305
ljs <artbiit@naver.com>
2024-10-06 20:17:41 +0900

    cors 적용

--- src/init/socket.js
+++ src/init/socket.js
@@ -2,8 +2,14 @@ import { Server as SocketIO } from 'socket.io';
 import registerHandler from '../handlers/register.handler.js';
 
 const initSocket = async (server) => {
-  const io = new SocketIO();
-  io.attach(server);
+  const io = new SocketIO(server, {
+    cors: {
+      origin: 'http://localhost:3000',
+      methods: ['GET', 'POST'],
+      allowedHeaders: ['my-custom-header'],
+      credentials: true,
+    },
+  });
 
   await registerHandler(io);
 };
